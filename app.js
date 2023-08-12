221991b0908e46db5ad1828a9887f6d1241ad9d9
Imed HAMDI <78560414+imedhamdi@users.noreply.github.com>
2023-08-12 17:15:09 +0200

    first commit
    initial

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const router = require('express').Router();
+var bodyParser = require('body-parser');
+const { log } = require('console');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('images'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+console.log("hello");
+
+const dbConn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+dbConn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!"); {
+        dbConn.query("CREATE DATABASE IF NOT EXISTS Groupomania", function (err, result) {
+            if (err) throw err;
+        })
+        dbConn.query("CREATE TABLE IF NOT EXISTS Groupomania.customers  (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))", function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    };
+});
+app.post('/', function (req, res) {
+    var user = req.body;
+    dbConn.query("USE Groupomania");
+    dbConn.query("INSERT INTO customers set ?", user, function (err, result) {
+
+        if (err) {
+            console.log(err);
+        } else {
+            res.send({
+                "code": 200,
+                "success": "L'utilisateur a été ajouté ! "
+            });
+        }
+    });
+
+});
+module.exports = app;
